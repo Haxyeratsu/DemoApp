f8e16a94c8de0678d0257fd4f980d79241a7b588
Anto' Musca <haxyeratsu@outlook.com>
2026-07-01 09:18:12 +0200

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: 'your-secret-key-change-in-production',
+    secret: '0101010101',
     resave: false,
     saveUninitialized: false,
     cookie: {
