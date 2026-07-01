f0f6b02ec66574c814c8be87e36f5da20bd63545
Anto' Musca <haxyeratsu@outlook.com>
2026-07-01 10:00:04 +0200

    bcrypt.js implementation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
